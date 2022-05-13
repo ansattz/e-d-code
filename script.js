562d42291d1e8770d28f8bf03396fe817020f42a
Douglas <dvieiras@aol.com>
2022-05-13 00:58:26 -0300

    update

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 const inputTexto = document.querySelector(".input-texto");
 const mensagem = document.querySelector(".mensagem");
 
-function btnEncripty() {
-    const txtEncriptado = encripty(inputTexto.value)
+function btnEncrypt() {
+    const txtEncriptado = encrypt(inputTexto.value)
     mensagem.value = txtEncriptado
     mensagem.style.backgroundImage="none"
 }
 
-function encripty(stringEncripted) {
+function encrypt(stringEncrypted) {
     let matrizCodigo = [["a","fgtx"], ["e","lgkx"], ["i","zasx"], ["o","xczx"], ["u","tadx"], ["d", "tjcx"], ["g", "qwx"], ["c", "szsx"], ["p", "ytx"] ];
-    stringEncripted = stringEncripted.toLowerCase();
+    stringEncrypted = stringEncrypted.toLowerCase();
 
     for(let i=0; i < matrizCodigo.length; i++) {
-        if(stringEncripted.includes(matrizCodigo[i][0])) {
-            stringEncripted = stringEncripted.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        if(stringEncrypted.includes(matrizCodigo[i][0])) {
+            stringEncrypted = stringEncrypted.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
         }
     }
-    return stringEncripted;
+    return stringEncrypted;
 }
 
-function btnDecripty() {
-    const txtDecripty = decripty(mensagem.value)
-    mensagem.value = txtDecripty
+function btnDecrypt() {
+    const txtdecrypt = decrypt(mensagem.value)
+    mensagem.value = txtdecrypt
 }
 
-function decripty(stringDecripted){
+function decrypt(stringDecrypted){
     let matrizDecode = [["fgtx","a"], ["lgkx","e"], ["zasx","i"], ["xczx","o"], ["tadx","u"], ["tjcx", "d"], ["qwx", "g"], ["szsx", "c"], ["ytx", "p"]];
 
     for(let i=0; i < matrizDecode.length; i++) {
-        if(stringDecripted.includes(matrizDecode[i][0])) {
-            stringDecripted = stringDecripted.replaceAll(matrizDecode[i][0], matrizDecode[i][1])
+        if(stringDecrypted.includes(matrizDecode[i][0])) {
+            stringDecrypted = stringDecrypted.replaceAll(matrizDecode[i][0], matrizDecode[i][1])
         }
     }
-    return stringDecripted;
+    return stringDecrypted;
 }
