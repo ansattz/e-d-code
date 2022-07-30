d5bcbc57128623c887c0a08fdf77feb583466b60
Douglas Santos <dvieiras@aol.com>
2022-07-30 13:27:43 -0300

    bigup

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-const txtInput = document.querySelector(".input-text");
-const msg = document.querySelector(".msg");
+const txtInput = document.querySelector(".text1");
+const msg = document.querySelector(".text-output");
 
 // Buttons functions
-    function btnEncrypt() {
-        const encryptTxt = encrypt(txtInput.value)
-        msg.value = encryptTxt
-        msg.style.backgroundImage="none"
-    }
+	function btnEncrypt() {
+		const encryptTxt = encrypt(txtInput.value)
+		msg.value = encryptTxt
+		msg.style.backgroundImage="none"
+	}
 
-    function encrypt(encryptedString) {
-        let codeMatrix = [["a","#xQ&&!"], ["e","&!@XA"], ["i","A&*FG"], ["o","&SDQ!!"], ["u","!WQWE$"], ["d", "E$@!!$%"], ["g", "$%27x"], ["c", "27y@!"], ["p", "243"] ];
-        encryptedString = encryptedString.toLowerCase();
+	function encrypt(encryptedString) {
+		let codeMatrix = [["a","#xQ&&!"], ["e","&!@XA"], ["i","A&*FG"], ["o","&SDQ!!"], ["u","!WQWE$"], ["d", "E$@!!$%"], ["g", "$%27x"], ["c", "27y@!"], ["p", "243"] ];
+		encryptedString = encryptedString.toLowerCase();
 
-        for(let i=0; i < codeMatrix.length; i++) {
-            if(encryptedString.includes(codeMatrix[i][0])) {
-                encryptedString = encryptedString.replaceAll(codeMatrix[i][0], codeMatrix[i][1])
-            }
-        }
-        return encryptedString;
-    }
+		for(let i=0; i < codeMatrix.length; i++) {
+				if(encryptedString.includes(codeMatrix[i][0])) {
+					encryptedString = encryptedString.replaceAll(codeMatrix[i][0], codeMatrix[i][1])
+				}
+		}
+		return encryptedString;
+	}
 
-    function btnDecrypt() {
-        const txtdecrypt = decrypt(msg.value)
-        msg.value = txtdecrypt
-    }
+	function btnDecrypt() {
+		const txtdecrypt = decrypt(msg.value)
+		msg.value = txtdecrypt
+	}
 
-    function decrypt(decryptedString){
-        let decodeMatrix = [["a","#xQ&&!"], ["e","&!@XA"], ["i","A&*FG"], ["o","&SDQ!!"], ["u","!WQWE$"], ["d", "E$@!!$%"], ["g", "$%27x"], ["c", "27y@!"], ["p", "243"] ];
+	function decrypt(decryptedString){
+		let decodeMatrix = [["a","#xQ&&!"], ["e","&!@XA"], ["i","A&*FG"], ["o","&SDQ!!"], ["u","!WQWE$"], ["d", "E$@!!$%"], ["g", "$%27x"], ["c", "27y@!"], ["p", "243"] ];
 
-        for(let i=0; i < decodeMatrix.length; i++) {
-            if(decryptedString.includes(decodeMatrix[i][1])) {
-                decryptedString = decryptedString.replaceAll(decodeMatrix[i][1], decodeMatrix[i][0])
-            }
-        }
-        return decryptedString;
-    }
+		for(let i=0; i < decodeMatrix.length; i++) {
+				if(decryptedString.includes(decodeMatrix[i][1])) {
+					decryptedString = decryptedString.replaceAll(decodeMatrix[i][1], decodeMatrix[i][0])
+				}
+		}
+		return decryptedString;
+	}
